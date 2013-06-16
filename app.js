1a1d0e13781dca43bb4bd19c08a99fb9b71c609f
Daniel Manary <dmanary@gmail.com>
2013-06-16 09:51:42 -0400

    Now using nodemailer, send mail from URL

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+var nodemailer = require("nodemailer");
 var express = require('express');
 //var fs = require('fs');
 //var http = require('https');
 var app = express();
+app.use(express.bodyParser()); // Required for parsing POST
+
+var transport = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
+    auth: {
+      user: "deventvo@gmail.com",
+      pass: "devent-vo"
+    }
+  });
 
 //var privateKey = fs.readFileSync('cert/server.key').toString();
-
+//var query = require('url').parse(req.url,true).query;
 // Routing
 app.use('/', express.static(__dirname + '/static'));
+
+function sendMailURI(req, res) {
+  var mailOptions = {
+      from: req.param('from'),
+      to: req.param('to'),
+      subject: req.param('subject'),
+      text: req.param('text')
+  }
+
+  console.log("Sending mail");
+  
+  /*var myResponse = {thank : "you"}; // Received JSON - need reply with JSON
+
+  res.writeHead( 200 );
+  res.setHeader("Content-Type", "text/html");
+  res.write(JSON.stringify(myResponse));
+  res.end();*/
+  
+  transport.sendMail(mailOptions);
+};
+
+app.get('/mail', sendMailURI); //mostly for testing purposes
+app.post('/mail', sendMailURI);
 
 // Set server listening
 app.listen(80);
